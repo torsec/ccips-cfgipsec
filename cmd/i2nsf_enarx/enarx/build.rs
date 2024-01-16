543acffe69e06f5db220ac6843eea4acb5ade11b
nuc <grazia.donghia@polito.it>
2024-01-16 12:00:13 +0100

    added modifications to test enarx app

--- cmd/i2nsf_enarx/enarx/build.rs
+++ cmd/i2nsf_enarx/enarx/build.rs
@@ -5,11 +5,13 @@ fn main()
 {
     let llvm_config_path = "/usr/bin/llvm-config";
     env::set_var("LLVM_CONFIG_PATH", llvm_config_path);
-    println!("cargo:rustc-link-search=native=/home/debian/ipsec_tests/ipsec_interactor/libs");
+//    println!("cargo:rustc-link-search=native=/home/debian/ipsec_tests/ipsec_interactor/libs");
+    println!("cargo:rustc-link-search=native=/home/nuc/SPIRS/TNED/ccips-cfgipsec/libs");
     println!("cargo:rustc-link-lib=static=i2nsf");
     println!("cargo:rustc-link-lib=static=parson");   
     let bindings = bindgen::Builder::default()
-        .header("/home/debian/ipsec_tests/ipsec_interactor/libs/trust_handler.h")
+//        .header("/home/debian/ipsec_tests/ipsec_interactor/libs/trust_handler.h")
+        .header("/home/nuc/SPIRS/TNED/ccips-cfgipsec/libs/trust_handler.h")
         .generate()
         .expect("Unable to generate bindings");
 
