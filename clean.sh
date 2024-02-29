21a4c04385a16d7447987e388010b91e4ac92f69
Giacomo Bruno <s301311@studenti.polito.it>
2024-02-29 15:22:06 +0100

    Added build scripts

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+rm -rf build
+rm -rf build_ra
+rm -rf build_ta
+rm -rf build_kl
+rm -rf build_ka
+rm -rf libs
+rm -rf cmd/i2nsf_enarx/enarx/target
---
+++ start_build_keystone.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+if [[ ! -v KEYSTONE_SDK_DIR ]]
+then
+    echo "KEYSTONE_SDK_DIR not set! Please set this to the location where Keystone SDK has been installed."
+    exit 1
+fi
+
+mkdir build_kl
+cd ./build_kl
+cmake -DKEYSTONE_LIB=ON ..
+make
+make install
+
+cd ..
+mkdir build_ka
+cd ./build_ka
+cmake -DKEYSTONE_APP=ON ..
+make
+make keystone-ccips-package
+cp keystone-ccips.ke $KEYSTONE_SDK_DIR/../../build/overlay/root/
+
+cd ..
+cp -r yang/ $KEYSTONE_SDK_DIR/../../build/overlay/root/
+cp install_yang_modules.sh $KEYSTONE_SDK_DIR/../../build/overlay/root/
